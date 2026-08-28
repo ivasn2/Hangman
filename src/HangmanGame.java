a66f799f2d541d05b3d4c38481b73c961c7109f8
ivasn2 <ivan.zharov0506@gmail.com>
2026-08-28 15:04:55 +0500

    refactor: decompose code into classes and methods following OOP principles

---
+++ src/HangmanGame.java
@@ -0,0 +1,32 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class HangmanGame {
+
+    private String randomItem;
+    private Set<Character> necessaryLetters;
+    private Set<Character> unNecessaryLetters;
+    private int mistakes;
+    private String result;
+    private StringBuilder stringBuilder;
+
+    public String buildMask() {
+        return "";
+    }
+
+    public void processLetter(int letter) {
+        return;
+    }
+
+    public String drawHangman() {
+        return "";
+    }
+
+    public boolean isWon() {
+        return false;
+    }
+
+    public boolean isLost() {
+        return false;
+    }
+}
